a43e8661ad3ac49bfb71ac514fac6cc2370c60d8
weiyi.li <weiyi.just2@gmail.com>
2018-08-11 18:00:37 +1000

    [ Hi Kotlin ] data class &  property comparison

---
+++ hikotlin/src/main/java/me/li2/android/hikotlin/kotlin/Test01.kt
@@ -0,0 +1,25 @@
+package me.li2.android.hikotlin.kotlin
+
+
+// Introduction to Kotlin (Google I/O '17)
+// https://www.youtube.com/watch?v=X1RVYt2QKQE
+//
+// Kotlin is now an officially supported language for Android. This session provides an introduction
+// to the language, covering basic concepts and tips for developers to get started with it.
+
+
+data class Money(val amount: Int, val currency: String)
+
+
+fun main(args: Array<String>) {
+
+    val ticket = Money(100, "$")
+    val popcorn = ticket.copy(amount = 200, currency = "$")
+
+    /* tip: this is doing is a property comparison one by one. It is not doing a point of comparison.
+     For point of comparisons, we have the triple equal. */
+    if (ticket != popcorn) {
+        println("They are different")
+    }
+    
+}
