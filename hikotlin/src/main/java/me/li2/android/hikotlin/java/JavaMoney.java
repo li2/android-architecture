b84f86242725be0181d89732ee7eb9e7f30718aa
weiyi.li <weiyi.just2@gmail.com>
2018-08-11 18:32:27 +1000

    [ Hi Kotlin ] for consuming Kotlin from Java, IDE will provide the getter/setter method instead of properties.

--- hikotlin/src/main/java/me/li2/android/hikotlin/java/JavaMoney.java
+++ hikotlin/src/main/java/me/li2/android/hikotlin/java/JavaMoney.java
@@ -1,5 +1,7 @@
 package me.li2.android.hikotlin.java;
 
+import me.li2.android.hikotlin.kotlin.Money;
+
 public class JavaMoney {
     private int amount;
     private String currency;
@@ -15,6 +17,14 @@ public class JavaMoney {
 
     public String getCurrency() {
         return currency;
+    }
+
+    /* tip: psvm will autocomplete main function. */
+    public static void main(String[] args) {
+        /* tip: for consuming Kotlin from Java, IDE will provide the getter/setter method
+         instead of properties. */
+        Money money = new Money(100, "$");
+        System.out.print("JavaMoney amount=" + money.getAmount());
     }
 
     @Override
