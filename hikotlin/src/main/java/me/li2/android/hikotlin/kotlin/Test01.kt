298bc2a8b1824740e787bee8ef55da315f970a25
weiyi.li <weiyi.just2@gmail.com>
2018-08-17 10:10:29 +1000

    [ Hi Kotlin ] return type by default is Unit, which is kind of like void, but it's not. It's actually a object which is essentially a singleton, a single instance of an object. And if it is a unit,  you don't have to put it there.

--- hikotlin/src/main/java/me/li2/android/hikotlin/kotlin/Test01.kt
+++ hikotlin/src/main/java/me/li2/android/hikotlin/kotlin/Test01.kt
@@ -13,10 +13,19 @@ import me.li2.android.hikotlin.java.JavaMoney
 data class Money(val amount: Int, val currency: String)
 
 
+/* tip: return type by default is Unit, which is kind of like void, but it's not. It's actually
+ a object which is essentially a singleton, a single instance of an object. And if it is a unit,
+ you don't have to put it there */
+fun sendPayment(money: Money, message: String = "") {
+    println("Sending money ${money.amount}")
+}
+
 fun main(args: Array<String>) {
 
     val ticket = Money(100, "$")
     val popcorn = ticket.copy(amount = 200, currency = "$")
+
+    sendPayment(ticket)
 
     /* tip: this is doing is a property comparison one by one. It is not doing a point of comparison.
      For point of comparisons, we have the triple equal. */
